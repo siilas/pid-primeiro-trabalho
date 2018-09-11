318ed8322ca12a23fdfaf3259a46aa1c50741c14
Silas Maciel Ferreira <s2it_sferreira@uolinc.com>
2018-09-11 13:57:29 -0300

    Setup do projeto e texturas

---
+++ src/main/java/com/github/siilas/pid/primeiro/trabalho/PidPrimeiroTrabalhoApplication.java
@@ -0,0 +1,13 @@
+package com.github.siilas.pid.primeiro.trabalho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PidPrimeiroTrabalhoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PidPrimeiroTrabalhoApplication.class, args);
+    }
+    
+}
